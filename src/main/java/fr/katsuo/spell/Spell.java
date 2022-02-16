937bd1fd0a163bec5668abc343720a271715a223
Katsuop <luka.deruyck236@gmail.com>
2022-02-16 23:41:52 +0100

    Initial commit

---
+++ src/main/java/fr/katsuo/spell/Spell.java
@@ -0,0 +1,16 @@
+package fr.katsuo.spell;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Spell extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
